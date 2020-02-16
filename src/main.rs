933fc4d04ad3642369b7aa5b3913ae1fb8843647
Beersonthewall <matthew@beersonthewall.com>
2020-02-15 21:20:58 -0600

    1.0.0 add command line args.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rand::random;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::env;
 
 #[derive(Hash, PartialEq, Eq, Debug, Clone)]
 enum Wall {
@@ -392,15 +393,33 @@ impl MazeBuilder {
     }
 }
 
+fn print_help() {
+    println!("Eller's maze generation algorithm implementation.");
+    println!("");
+    println!("USAGE:");
+    println!("      gen [width] [iterations]");
+    std::process::exit(1);
+}
+
 fn main() {
-    let mut maze_bldr = MazeBuilder::new(10);
-    maze_bldr.print_row();
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 && (args[1] == "--help" || args[1] == "-help") {
+        print_help();
+    }
+    if args.len() != 3 {
+        println!("Error");
+    }
+    let width = usize::from_str_radix(&args[1], 10).unwrap();
+    let iterations = usize::from_str_radix(&args[2], 10).unwrap();
 
-    maze_bldr.ellers();
-    maze_bldr.print_row();
+    let mut builder = MazeBuilder::new(width);
 
-    maze_bldr.end();
-    maze_bldr.print_row();
+    for _ in 0..iterations - 1 {
+        builder.ellers();
+        builder.print_row();
+    }
+    builder.end();
+    builder.print_row();
 }
 
 #[cfg(test)]
